7cbd722831d322bc234de1c115db74c886d4bd94
VictorHemengul <86350703+VictorHemengul@users.noreply.github.com>
2021-12-03 19:54:42 +0500

    created admin add create quiz

--- handlers/user/admin.py
+++ handlers/user/admin.py
@@ -1,40 +1,68 @@
 from aiogram import types
+from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from loader import dp
+from keyboards.common import btn_create_quiz
+from loader import dp, bot
+from aiogram.dispatcher.filters import BoundFilter
+from setting.config import admin_id
 
 
-class FSMAmin(StatesGroup):
+class CheackAdmin(BoundFilter):
+	key = admin_id
+	
+	async def check(self, message: types.Message):
+		member = await bot.get_chat_member(message.chat.id, message.from_user.id)
+		return member.is_chat_admin()
+
+
+class FSMAmdin(StatesGroup):
 	name = State()
 	description = State()
+	
 
+class FSMAdmincreatedQuzi(StatesGroup):
+	name = State()
+	description = StatesGroup
+	
 
-@dp.message_hendler(commands='Загрузить', state=None)
-async def cm_start(message: types.Message):
-	await FSMAmin.name.set()
-	await message.reply('Введи название')
+@dp.message_handler(commands='create')
+async def create_quiz(message: types.Message):
+	await message.answer("Выберете один их предложенных вариантов", reply_markup=btn_create_quiz.create_quiz)
+	
 
-
-# @dp.message_handler(content_types=['photo'], state=FSMAmin.photo)
-# async def load_photo(message: types.Message, state: FSMContext):
-# 	async with state.proxy() as data:
-# 		data['photo'] =message.photo[0].file_id
-# 	await FSMAmin.next()
-# 	await message.reply("Tеперь введи назварние")
+@dp.message_handler(Text(equals="Создать опрос"))
+async def add_name_quiz(message: types.Message):
+	await FSMAdmincreatedQuzi.name.set()
+	await message.reply("Введите название опроса")
 	
 	
-@dp.message_handler(state=FSMAmin.description)
-async def load_name(message: types.Message, state: FSMContext):
+@dp.message_handler(state=FSMAdmincreatedQuzi.name)
+async def add_description(message: types.Message, state: FSMContext):
 	async with state.proxy() as data:
 		data['name'] = message.text
-	await FSMAmin.next()
-	await message.reply("Введи описание")
+	await FSMAdmincreatedQuzi.next()
+	await message.reply("Введите описание опроса")
 
 
-@dp.message_handler(state=FSMAmin.description)
-async def load_description(message: types.Message, state: FSMContext):
+@dp.message_handler(state=FSMAdmincreatedQuzi.description)
+async def load_quiz(message: types.Message, state=None):
 	async with state.proxy() as data:
-		data['discription'] = message.text
-	await state.finish()
+		data['position'] = message.text
+	await message.answer("Quiz успешно добавлен")
 	
 	
+@dp.message_handler(lambda message: message.text == "Отмена")
+async def action_cancel(message: types.Message):
+	remove_btm = types.ReplyKeyboardRemove()
+	await message.answer("Если хотите опять добавить quiz и вопросы напишите /create", reply_markup=remove_btm)
+
+
+def register_handlers_use(dp: Dispatcher):
+	"""
+		регистрация handlers
+	"""
+	dp.register_message_handler(create_quiz, commands='sign', state=None)
+	dp.register_message_handler(add_description, state=FSMAdmincreatedQuzi.name)
+	dp.register_message_handler(load_quiz, state=FSMAdmincreatedQuzi.description)
